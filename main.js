c241954d4c9533e19093a598b36e0df16f6d8e5f
ahgomes <adrianhgomes@gmail.com>
2016-03-14 22:21:49 -0400

    animations added

---
+++ main.js
@@ -0,0 +1,9 @@
+function loading() {
+    document.getElementById('headLogo').style.top = 'calc(50% - 86.5px)';
+    document.getElementById('pillarI').style.top = 'calc(50% + 100px)';
+    document.getElementById('pillar0').style.top = 'calc(50% - 300px)';
+    document.getElementById('pillar1').style.top = 'calc(50% - 300px)';
+
+
+    document.getElementById('headText').style.color = '#fff';
+}
